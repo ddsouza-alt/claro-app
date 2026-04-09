f806fd30a51274f192bd3352ffa3a59df4f3ac1a
ddsouza-alt <ddsouza@theservitium.com>
2026-04-09 13:24:29 -0700

    Initialize main.jsx with React and Claro component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Claro from './Claro.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Claro />
+  </React.StrictMode>
+)
